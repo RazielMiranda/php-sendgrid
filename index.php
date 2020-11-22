25448770582fef5541a62e4b24eea87e59990aac
Raziel M <raziel@upoutsourcing.com>
2020-11-22 18:27:29 -0300

    finish code of curl

--- index.php
+++ index.php
@@ -1,3 +1,51 @@
 <?php
 
-$key = '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';
+$email = "razielx3@live.com";
+$name = "RZ";
+$body = "<h1>hello</h1>";
+$subject = "hello";
+$key = "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";
+$headers = array(
+    "Authorization: Bearer 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",
+    'Content-Type: application/json'
+);
+
+$data = array(
+    "personalizations" => array(
+        array(
+            "to" => array(
+                array(
+                    "email" => $email,
+                    "name" => $name
+                )
+            )
+        )
+    ),
+    "from" => array(
+        "email" => "hello@mycode.com"
+    ),
+    "subject" => $subject,
+    "content" => array(
+        array(
+            "type" => "text/html",
+            "value" => $body
+        )
+    )
+);
+
+$ch = curl_init();
+curl_setopt($ch, CURLOPT_URL, "https://api.sendgrid.com/v3/mail/send");
+curl_setopt($ch, CURLOPT_POST, 1);
+curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($data));
+curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+$response = curl_exec($ch);
+curl_close($ch);
+
+echo $response;
+// curl --request POST \
+//   --url https://api.sendgrid.com/v3/mail/send \
+//   --header "Authorization: Bearer $SENDGRID_API_KEY" \
+//   --header 'Content-Type: application/json' \
+//   --data '{"personalizations": [{"to": [{"email": "test@example.com"}]}],"from": {"email": "test@example.com"},"subject": "Sending with SendGrid is Fun","content": [{"type": "text/plain", "value": "and easy to do anywhere, even with cURL"}]}'

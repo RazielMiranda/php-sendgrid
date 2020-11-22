11a8d24127e53290004dd2593eaf07a61fca1d97
Raziel M <raziel@upoutsourcing.com>
2020-11-22 18:00:51 -0300

    commit

--- index.php
+++ index.php
@@ -1,2 +1,20 @@
 <?php
-require_once '<PATH TO>/sendgrid-php.php';
+require_once 'sendgrid-php/sendgrid-php.php';
+
+$email = new \SendGrid\Mail\Mail();
+$email->setFrom("razielx3@live.com", "Raziel");
+$email->setSubject("Sending with Twilio SendGrid is Fun");
+$email->addTo("razielx3@live.com", "Raziel");
+$email->addContent("text/plain", "and easy to do anywhere, even with PHP");
+$email->addContent(
+    "text/html", "<strong>and easy to do anywhere, even with PHP</strong>"
+);
+$sendgrid = new \SendGrid('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');
+try {
+    $response = $sendgrid->send($email);
+    print $response->statusCode() . "\n";
+    print_r($response->headers());
+    print $response->body() . "\n";
+} catch (Exception $e) {
+    echo 'Caught exception: '. $e->getMessage() ."\n";
+}
